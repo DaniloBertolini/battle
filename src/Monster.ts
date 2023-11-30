a1f7588e1899b1065ad74010cbf107b1dea49d91
Danilo <danilocbertolini@gmail.com>
2023-11-30 10:14:49 -0300

    Criação da Classe Monstro

---
+++ src/Monster.ts
@@ -0,0 +1,39 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) {
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this.calculateDamage(attackPoints);
+    this.isAlive();
+
+    return this.lifePoints;
+  }
+
+  private calculateDamage(damage: number) {
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    } else {
+      this._lifePoints -= 1;
+    }
+  }
+
+  private isAlive(): void {
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
